91d38b5d048ca6891d07316d2eec2a09e72a33d0
Paulo Choi <mail@paulochoi.com>
2015-03-07 19:38:15 -0500

    Initial Commit

---
+++ KindNest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindNest
+//
+//  Created by Paulo Choi on 3/7/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
